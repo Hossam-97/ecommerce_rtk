6052cb4a97d775079210d6762bacbcc6b48bda4c
Hossam-97 <hossam_1997@live.com>
2025-04-09 14:56:46 -0400

    adding lab changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-// App.js
 import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
-import './App.css'
-const App = () => {
-  return (
- 
-      <div>
-        <h1 className='app-heading'>E-Commerce Application</h1>
-        <ProductList />
-        <ShoppingCart />
-      </div>
+import './App.css';
+import SuperCoin from './Components/SuperCoin'; // Import SuperCoin
 
-  );
+const App = () => {
+    return (
+        <div><h1 className='app-heading'>E-Commerce Application</h1><ProductList /><ShoppingCart /><SuperCoin /> {/* Include the SuperCoin component here */}
+        </div>
+    );
 };
 
 export default App;
